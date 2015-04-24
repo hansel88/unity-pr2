ca9ca4dd6fa4884bd0156660546530aa63f9d55d
TordTrøen <assessedgrub@gmail.com>
2015-04-24 12:54:51 +0200

    La til funksjoner i GUIManager

--- Super Mario Unity/Assets/Scripts/GUIManager.cs
+++ Super Mario Unity/Assets/Scripts/GUIManager.cs
@@ -1,12 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GUIManager : MonoBehaviour
 {
+	public Text textScore;
+	public Text textCoin;
+	public Text textTime;
+	public Text textLevel;
+
 	public static GUIManager instance;
 
 	void Awake()
 	{
 		instance = this;
+	}
+
+	public int ChangeScoreText(int score)
+	{
+		textScore.text = string.Format ("{0}", score);
+	}
+
+	public int ChangeCoinText(int coins)
+	{
+		textCoin.text = string.Format ("{0}", coins);
+	}
+
+	public int ChangeTimeText(int time)
+	{
+		textTime.text = string.Format ("{0}", time);
+	}
+
+	public int ChangeLevelText(string level)
+	{
+		textLevel.text = string.Format ("{0}", level);
 	}
 }
