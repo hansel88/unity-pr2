93d3f181d383b0e0d2905c8bef69c76195c2c05c
hansel88 <hpn_x@hotmail.com>
2015-04-24 12:21:49 +0200

    added empty scripts, updated GM

--- Super Mario Unity/Assets/Scripts/GM.cs
+++ Super Mario Unity/Assets/Scripts/GM.cs
@@ -5,6 +5,7 @@ public class GM : MonoBehaviour
 {
     public static GM instance = null;
 
+    #region score, lives and coins properties
     private int score = 0;
     public int Score
     {
@@ -25,6 +26,40 @@ public class GM : MonoBehaviour
         get { return this.coins; }
         set { this.coins = value; }
     }
+    #endregion
+
+    #region status and powerup properties
+
+    public enum MarioStatus { Big, Small };
+
+    private MarioStatus characterStatus;
+    public MarioStatus CharacterStatus
+    {
+        get { return this.characterStatus; }
+        set { this.characterStatus = value; }
+    }
+
+    private bool hasFireFly = false;
+    public bool HasFireFly
+    {
+        get { return this.hasFireFly; }
+        set { this.hasFireFly = value; }
+    }
+
+    private bool hasStar = false;
+    public bool HasStar
+    {
+        get { return this.hasStar; }
+        set { this.hasStar = value; }
+    }
+
+    private bool hasMushroom = false;
+    public bool HasMushroom
+    {
+        get { return this.hasMushroom; }
+        set { this.hasMushroom = value; }
+    }
+#endregion
 
     void Awake()
     {
@@ -35,5 +70,11 @@ public class GM : MonoBehaviour
 
     }
 
+    #region methods
+    private void checkGameOver()
+    {
+        //TODO
+    }
+    #endregion
 
 }
