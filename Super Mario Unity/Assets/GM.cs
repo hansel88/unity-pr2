c4774588c929e495a6e1b3d3631c5e399a74c1ba
hansel88 <hpn_x@hotmail.com>
2015-04-24 11:56:59 +0200

    added GM script

---
+++ Super Mario Unity/Assets/GM.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Timers;
+using System;
+
+public class GM : MonoBehaviour
+{
+    public static GM instance = null;
+
+    private int score = 0;
+    public int Score
+    {
+        get { return this.score; }
+        set { this.score = value; }
+    }
+
+    // Use this for initialization
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+    }
+
+
+}
---
+++ Super Mario Unity/Assets/Scripts/GM.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GM : MonoBehaviour
+{
+    public static GM instance = null;
+
+    private int score = 0;
+    public int Score
+    {
+        get { return this.score; }
+        set { this.score = value; }
+    }
+
+    private int lives = 0;
+    public int Lives
+    {
+        get { return this.lives; }
+        set { this.lives = value; }
+    }
+
+    private int coins = 0;
+    public int Coins
+    {
+        get { return this.coins; }
+        set { this.coins = value; }
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+    }
+
+
+}
