e0f9d08ca0f4a50263e278eb124dce464a309254
TordTrøen <assessedgrub@gmail.com>
2015-04-25 02:23:49 +0200

    Added enemy jumping

--- Super Mario Unity/Assets/Scripts/Enemy.cs
+++ Super Mario Unity/Assets/Scripts/Enemy.cs
@@ -3,5 +3,29 @@ using System.Collections;
 
 public class Enemy : Entity
 {
+	public bool jump = false;
+	public float jumpForce = 100f;
+	public float jumpRate = 1f;
+	private float curJumpTimer;
+	private Rigidbody2D rBody;
 
+	void Awake()
+	{
+		rBody = GetComponent<Rigidbody2D>();
+	}
+
+	void Update()
+	{
+		HorizontalMovement ();
+
+		if (jump)
+		{
+			curJumpTimer += Time.deltaTime;
+			if (curJumpTimer >= jumpRate)
+			{
+				rBody.AddForce (Vector2.up * jumpForce);
+				curJumpTimer = 0f;
+			}
+		}
+	}
 }
--- Super Mario Unity/Assets/Scripts/Entity.cs
+++ Super Mario Unity/Assets/Scripts/Entity.cs
@@ -3,19 +3,52 @@ using System.Collections;
 
 public class Entity : MonoBehaviour
 {
-	public float movementSpeed = 10f;
+	public float movementSpeed = 3f;
 	public int direction = 1;
+	public Transform spriteTransform; // The transform to rotate when turning
 
-	public void Movement()
+	private bool isChangingDirection = false;
+
+	void Start()
 	{
-		transform.Translate ((transform.right * movementSpeed * Time.deltaTime) * direction);
+		// Use the first child as a spritetransform incase nothing is assigned
+		if (!spriteTransform)
+		{
+			Debug.LogWarning ("Assigning first child as spritetransform because none were assigned.", this);
+			spriteTransform = transform.GetChild (0);
+		}
 	}
 
-	void OnTriggerEnter2D(Collider2D other)
+	public void HorizontalMovement()
 	{
-		if (!other.CompareTag ("Ground"))
+		// Moves the object in a horizontal direction
+		transform.Translate ((transform.right * direction) * movementSpeed * Time.deltaTime);
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (!other.collider.CompareTag ("Ground"))
 		{
-			direction *= -1;
+			// Check if the collided object is on the correct side
+			Vector3 otherPos = other.transform.position;
+			Vector3 pos = transform.position;
+			if ((otherPos.x > pos.x && direction == 1) || (otherPos.x < pos.x && direction == -1))
+			{
+				// Check if we are already changing direction before turning
+				if (!isChangingDirection)
+				{
+					StartCoroutine (ChangeDirection ());
+				}
+			}
 		}
+	}
+	
+	IEnumerator ChangeDirection()
+	{
+		isChangingDirection = true;
+		yield return new WaitForEndOfFrame();
+		direction *= -1;
+		spriteTransform.localScale = new Vector3(direction, 1f);
+		isChangingDirection = false;
 	}
 }
