fec300422f919879d48b2834c5842203588195c4
hansel88 <hpn_x@hotmail.com>
2015-04-27 12:55:14 +0200

    basic player movement..

--- Super Mario Unity/Assets/Scripts/CharacterMovement.cs
+++ Super Mario Unity/Assets/Scripts/CharacterMovement.cs
@@ -1,15 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Movement : MonoBehaviour {
+public class CharacterMovement : MonoBehaviour {
+
+
+    float speed = 1.0f;
+    float jumpForce = 200.0f;
+    private bool grounded = true;
 
 	// Use this for initialization
 	void Start () {
 	
 	}
+
+
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if ((Input.GetKeyDown("space") || Input.GetKeyDown("up")) && grounded == true)
+        {
+            grounded = false;
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+        else
+        {
+            var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+            transform.position += move * speed * Time.deltaTime;
+        }
+
 	}
+
+    void OnCollisionEnter2D(Collision2D colObj)
+    {
+        if (colObj.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
+
 }
