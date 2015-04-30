1b1aaf9ed74b7431c40b5d0e0ca3de9dc0b6a19c
hansel88 <hpn_x@hotmail.com>
2015-04-30 16:19:22 +0200

    cameracont updated

--- Super Mario Unity/Assets/Scripts/CameraController.cs
+++ Super Mario Unity/Assets/Scripts/CameraController.cs
@@ -20,10 +20,18 @@ public class CameraController : MonoBehaviour {
     void TrackPlayer()
     {
         // By default the target x and y coordinates of the camera are it's current x and y coordinates.
+
+        
         float targetX = Player.position.x;
+
+        GM.instance.UpdateScreenBounds();
+        if(targetX > GM.instance.camWorldCenter.x)
+        {
+            this.transform.position = new Vector3(targetX, this.transform.position.y, transform.position.z);
+        }
         //float targetY = Player.position.y;
 
         // Set the camera's position to the target position with the same z component.
-        this.transform.position = new Vector3(targetX, this.transform.position.y, transform.position.z);
+
     }
 }
