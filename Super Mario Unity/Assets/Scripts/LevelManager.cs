bcbc69e4cc60ac3ac83e7e8692c44ce834f4de01
TordTrøen <assessedgrub@gmail.com>
2015-05-11 13:07:12 +0200

    Added levelmanager functions

--- Super Mario Unity/Assets/Scripts/LevelManager.cs
+++ Super Mario Unity/Assets/Scripts/LevelManager.cs
@@ -3,6 +3,17 @@ using System.Collections;
 
 public class LevelManager : MonoBehaviour 
 {
+	public static LevelManager instance;
+
+	void Awake()
+	{
+		instance = this;
+	}
+
+	public void LoadLevel(int worldId, int levelId)
+	{
+
+	}
 
 	public void OnStageStart()
 	{
