82f019cc784e28f2de3bb6f1f26b83ecd87702d7
hansel88 <hpn_x@hotmail.com>
2015-05-13 13:33:18 +0200

    coin pickup sound added

--- Super Mario Unity/Assets/Scripts/CoinPickup.cs
+++ Super Mario Unity/Assets/Scripts/CoinPickup.cs
@@ -3,10 +3,13 @@ using System.Collections;
 
 public class CoinPickup : MonoBehaviour
 {
+
+    public GameObject coinPickupSound;
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.CompareTag (Tags.player))
 		{
+            Destroy(GameObject.Instantiate(coinPickupSound), 2);
 			GM.instance.Coins ++;
 			gameObject.SetActive (false);
 		}
