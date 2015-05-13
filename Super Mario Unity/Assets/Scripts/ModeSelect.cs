db1b1981c18ef686e95005fd3a7962dd6a00f8f4
Ibenoliver <ibenoliver@hotmail.com>
2015-05-13 18:10:14 +0200

    Main Title

---
+++ Super Mario Unity/Assets/Scripts/ModeSelect.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ModeSelect : MonoBehaviour {
+
+    public int playerAmount;
+    
+
+	// Use this for initialization
+	void Start () {
+        playerAmount = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("down"))
+        {
+            playerAmount = 2;
+            this.transform.position = new Vector3(-4, -1, 0);
+        }
+
+        if (Input.GetKey("up"))
+        {
+            playerAmount = 1;
+            this.transform.position = new Vector3(-4, 0, 0);
+        }
+
+        if (Input.GetButtonDown("Submit"))
+        {
+            Application.LoadLevel("Level_01");
+        }
+	}
+}
